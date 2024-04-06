107f4e6c19b84a39eda617663f5c19677db86a63
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-06 17:06:05 +0300

    Initial commit

---
+++ src/main/java/com/springworks/springworld/SpringWorldApplication.java
@@ -0,0 +1,13 @@
+package com.springworks.springworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWorldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springworks/springworld/SpringWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springworks.springworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
