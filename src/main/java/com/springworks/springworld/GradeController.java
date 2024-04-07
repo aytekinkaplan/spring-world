07aec74d00d6ca93c41461cfb5c4febaf23d689f
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-07 07:14:14 +0300

    Spring Boot Commit

---
+++ src/main/java/com/springworks/springworld/GradeController.java
@@ -0,0 +1,17 @@
+package com.springworks.springworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GradeController {
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        Grade grade = new Grade("John", "Math", "100");
+
+        model.addAttribute("grade", grade);
+
+        return "grades";
+    }
+}
--- src/main/java/com/springworks/springworld/GradeSubmissionApplication.java
+++ src/main/java/com/springworks/springworld/GradeSubmissionApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWorldApplication {
+public class GradeSubmissionApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringWorldApplication.class, args);
+        SpringApplication.run(GradeSubmissionApplication.class, args);
+
     }
 
 }
