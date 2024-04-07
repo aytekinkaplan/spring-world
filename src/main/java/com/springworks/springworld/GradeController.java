7769d6b04e650ff68faa694cb7e17f1c65a72ddf
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-07 08:53:31 +0300

    Spring Boot Commit

--- src/main/java/com/springworks/springworld/GradeController.java
+++ src/main/java/com/springworks/springworld/GradeController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GradeController {
     @GetMapping("/grades")
     public String getGrades(Model model) {
-        Grade grade = new Grade("John", "Math", "100");
+        //Grade grade = new Grade("John", "Math", "100");
 
-        model.addAttribute("grade", grade);
+        model.addAttribute("grade1", new Grade("John", "Math", "100"));
+        model.addAttribute("grade2", new Grade("Marry", "Physics", "90"));
+        model.addAttribute("grade3", new Grade("Harry", "Chemistry", "80"));
 
         return "grades";
     }
